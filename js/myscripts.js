d426b16956cb1d8c8823a123055477fc7606b1c0
JaiChandwani <92116987+JaiChandwani@users.noreply.github.com>
2023-12-03 23:01:47 +0530

    Initial files are uploded.
    All the HTML,CSS,JAVASCRIPT files has been uploded sucessfully.

---
+++ js/myscripts.js
@@ -0,0 +1,15 @@
+// scroll down banner button
+document.getElementById("scrollBtn").addEventListener("click", function() {
+    window.scrollBy(0, 577);
+    });
+
+// scroll navbar title
+document.getElementById("abtBtn").addEventListener("click",function(){
+    window.scrollBy(0,100)
+})
+
+const element = document.getElementById("prgBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("prgressbarAnimate")}
